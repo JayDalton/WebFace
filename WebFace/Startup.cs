3647780f7a0a64e720dc94ec482ae3cf7552a1b4
Thomas Gerlach <s0531712@htw-berlin.de>
2015-02-03 00:51:10 +0100

    Initial Commit

---
+++ WebFace/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFace.Startup))]
+namespace WebFace
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
